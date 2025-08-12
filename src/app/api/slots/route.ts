105852701a1e645ef7caf0e2766677d49a4196a6
kryeziunebih-code <kryeziu.nebih@gmail.com>
2025-08-12 15:44:14 +0200

    Update route.ts

--- src/app/api/slots/route.ts
+++ src/app/api/slots/route.ts
@@ -1,5 +1,9 @@
+// src/app/api/slots/route.ts
+
 import { prisma } from "../../../lib/prisma";
 import { NextResponse } from "next/server";
+
+export const dynamic = 'force-dynamic'; // <-- ADD THIS LINE
 
 export async function GET() {
   const slots = await prisma.slot.findMany({ include: { clinic: true, provider: true }, orderBy: { startsAt: "asc" } });
