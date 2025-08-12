500d66b697b4cf47ee2ce587e62ae13df6345fb5
kryeziunebih-code <kryeziu.nebih@gmail.com>
2025-08-12 15:42:59 +0200

    Update route.ts

--- src/app/api/clinics/route.ts
+++ src/app/api/clinics/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'; // <-- ADD THIS LINE
+
 export async function GET() {
   const clinics = await prisma.clinic.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(clinics);
